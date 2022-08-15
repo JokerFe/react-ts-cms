b5df58cbfc36ebb0ced946d5e3499b08033ecd51
cuihaoran <1054346513@qq.com>
2022-08-15 09:57:08 +0800

    feat(wdyr.ts): 忽略eslint检查

--- src/wdyr.ts
+++ src/wdyr.ts
@@ -1,9 +1,17 @@
+/*
+ * @Author: 崔浩然
+ * @Email：cuihaoran@hualala.com
+ * @Date: 2022-08-02 10:31:29
+ * @Description: 页面/组件/功能的描述
+ * @FilePath: /hr-react-web/src/wdyr.ts
+ */
 /// <reference types="@welldone-software/why-did-you-render" />
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
-  // const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  // whyDidYouRender(React, {
-  //   trackAllPureComponents: true,
-  // });
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
 }
